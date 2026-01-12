4d7471e669d2971802858c7e6799e4df7a480b7b
hairenjun <hairenjun@hotmail.com>
2026-01-12 01:58:32 -0800

    fix render permission issue on Nvidia driver, add high resolution icon

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,10 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+  std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+  std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+
+  app_lib::run();
+
+}
